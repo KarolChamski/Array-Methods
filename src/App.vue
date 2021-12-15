0004dd9ef828edaaea04de432ff879410d8715b0
Karol <karol.chamski@gmail.com>
2021-12-15 12:32:02 +0100

    Added filter and sort methods

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-
-  <methods-bar :result="result" @map="map"></methods-bar>
-  <the-characters :characters="characters" ></the-characters>
+  <methods-bar @filter="filter" :result="result" @map="map" @sort="sort"></methods-bar>
+  <the-characters :characters="updatedCharacters" ></the-characters>
 
 
 </template>
@@ -26,74 +25,113 @@ export default {
           height: 2,
           gifts: 23,
           dressColor: "green",
-          img: 'Micky.png'
+          img: 'Micky.png',
+          gender: "male"
         },
         {
           name: "Ollie",
           height: 1,
           gifts: 8,
           dressColor: "green",
-          img: 'Ollie.png'
+          img: 'Ollie.png',
+          gender: "male"
         },
         {
           name: "Elly",
           height: 3,
           gifts: 30,
           dressColor: "green",
-          img: 'Elly.png'
+          img: 'Elly.png',
+          gender: "female"
         },
         {
           name: "Santa",
           height: 4,
           gifts: 100,
           dressColor: "red",
-          img: 'Santa.png'
+          img: 'Santa.png',
+          gender: "male"
         },
           {
             name: "Mrs. Santa",
             height: 4,
             gifts: 99,
             dressColor: "red",
-            img: 'Mrs.Santa.png'
+            img: 'Mrs.Santa.png',
+            gender: "female"
           },
         {
           name: "Lili",
           height: 1,
           gifts: 26,
           dressColor: "green",
-          img: 'Lili.png'
+          img: 'Lili.png',
+          gender: "female"
         },
         {
           name: "Sophia",
           height: 3,
           gifts: 65,
           dressColor: "green",
-          img: 'Sophia.png'
+          img: 'Sophia.png',
+          gender: "female"
         },
         {
           name: "Mia",
           height: 2,
           gifts: 23,
           dressColor: "green",
-          img: 'Mia.png'
+          img: 'Mia.png',
+          gender: "female"
         },
         {
           name: "John",
           height: 3,
           gifts: 45,
           dressColor: "green",
-          img: 'John.png'
+          img: 'John.png',
+          gender: "male"
         },
-      ]
+      ],
+      updatedCharacters: [],
+      
     }
   },
   methods:{
+    updateCharacters(){
+    this.updatedCharacters = this.characters
+    },
     map(){
       const mapArray = this.characters.map(character => character.name);
       this.result = "Names of characters: " +  mapArray
     },
+    filter(gender){
+      const filterArray = this.characters.filter(character => character.gender == gender);
+      this.updatedCharacters = filterArray;
+    },
+    sort(property){
+      if(property == "height"){
+        this.characters.sort((a,b)=>  a.height - b.height);
+      }
+      else if(property == "name"){
+        this.characters.sort((a,b)=>{
+          if(a.name<b.name){
+            return -1
+          } else{
+            return 1
+          }
+        })
+      }
+      else{
+        this.characters.sort((a,b)=> a.gifts - b.gifts)
+      }
+    },
 
-  }
+  },
+
+    mounted(){
+    this.updateCharacters()
+    }
 }
 </script>
 
--- src/components/MethodsBar.vue
+++ src/components/MethodsBar.vue
@@ -1,26 +1,53 @@
 <template>
 <div class="wrapper">
     <div class="buttons-bar">
-        <button @click="$emit('map')">Map</button>
-        <button>Filter</button>
-        <button>Sort</button>
+
+        <button @click="showMapOptions">Map</button>
+        <button @click="showFilterOptions">Filter</button>
+        <button @click="showSortOptions">Sort</button>
         <button>Every</button>
         <button>Some</button>
         <button>Reduce</button>
     </div>
     <div class="action-box">
+        <div class="choose-box">
+            <button v-if="mapOptions" @click="$emit('map')">Names of Characters</button>
+            <button @click="$emit('filter', 'female')" v-if="filterOptions">Girls</button>
+            <button @click="$emit('filter', 'male')" v-if="filterOptions">Boys</button>
+            <button v-if="sortOptions" @click="$emit('sort', 'height')">By Height</button>
+            <button v-if="sortOptions" @click="$emit('sort', 'name')">By Names</button>
+            <button v-if="sortOptions" @click="$emit('sort', 'gifts')">By gifts given</button>
+
+            
+        </div>
         <div class="result-box">
             <p>{{result}}</p>
-        </div>
-        <div class="choose-box">
-            
         </div>
     </div>
 </div>
 </template>
 <script>
 export default{
-    props: ['result']
+    props: ['result'],
+    data(){
+        return{
+            mapOptions: false,
+            filterOptions: false,
+            sortOptions: false
+        }
+    },
+    methods:{
+        showMapOptions(){
+            this.mapOptions = !this.mapOptions
+        },
+        showFilterOptions(){
+            this.filterOptions = !this.filterOptions
+        },
+        showSortOptions(){
+            this.sortOptions = !this.sortOptions
+        }
+
+    }
 }
 </script>
 <style scoped>
@@ -30,6 +57,11 @@ export default{
     }
     .action-box{
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+    .method-box{
+        display: flex;
+        flex-direction: column;
     }
 </style>
